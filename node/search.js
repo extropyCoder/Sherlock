69af4a51ff9f2c6be1b8ac47387d4cb694d8ea7c
Laurence Kirk <laurence@extropy.io>
2016-08-25 13:43:40 +0100

    new js

--- node/search.js
+++ node/search.js
@@ -1,5 +1,32 @@
 var Web3 = require("web3");
-function getTransactionsByAccount(myaccount, startBlockNumber, endBlockNumber, nonce) {
+var trackAddress = ["0x304a554a310c7e546dfe434669c62820b7d83490","0x304a554a310c7e546dfe434669c62820b7d83490","0xe592ac96747db16337ac8f2e9d1e3dc8fd8bcca7"];
+
+function saveAndDisplay(block,transaction){
+addToArray(transaction.from);
+addToArray(transaction.to);
+
+  console.log("  tx hash          : " + transaction.hash + "\n"
+  + "   nonce           : " + transaction.nonce + "\n"
+  + "   blockHash       : " + transaction.blockHash + "\n"
+  + "   blockNumber     : " + transaction.blockNumber + "\n"
+  + "   transactionIndex: " + transaction.transactionIndex + "\n"
+  + "   from            : " + transaction.from + "\n"
+  + "   to              : " + transaction.to + "\n"
+  + "   value           : " + transaction.value + "\n"
+  + "   time            : " + block.timestamp + " " + new Date(block.timestamp * 1000).toGMTString() + "\n"
+  + "   gasPrice        : " + transaction.gasPrice + "\n"
+  + "   gas             : " + transaction.gas + "\n"
+  + "   input           : " + transaction.input);
+
+}
+
+function addToArray(address){
+  if (trackAddress.indexOf(address) != -1) {
+    trackAddress.push(address);
+  }
+}
+
+function getTransactionsForAccount(startBlockNumber, endBlockNumber) {
   if (endBlockNumber == null) {
     endBlockNumber = eth.blockNumber;
     console.log("Using endBlockNumber: " + endBlockNumber);
@@ -8,7 +35,7 @@ function getTransactionsByAccount(myaccount, startBlockNumber, endBlockNumber, n
     startBlockNumber = endBlockNumber - 1000;
     console.log("Using startBlockNumber: " + startBlockNumber);
   }
-  console.log("Searching for transactions to/from account \"" + myaccount + "\" within blocks "  + startBlockNumber + " and " + endBlockNumber);
+  console.log("Searching for transactions to/from account within blocks "  + startBlockNumber + " and " + endBlockNumber);
 
   for (var i = startBlockNumber; i <= endBlockNumber; i++) {
      if (i % 1000 == 0) {
@@ -18,27 +45,12 @@ function getTransactionsByAccount(myaccount, startBlockNumber, endBlockNumber, n
     if (block != null && block.transactions != null) {
         //console.log("got trans");
         block.transactions.forEach( function(e) {
-          var fromAddress  = e.from;
-//          if (e.hash=="0xf6dc750d6ce1c710a18482cc1b37e82f0636d149dca8078c1154a7a9840fa3b1")   { //&& nonce == e.transactionIndex) {
-            if (e.from=="0x05694d29b7c6c65abce2a7c45317d0e177339d35")   { //&& nonce == e.transactionIndex) {
 
-        //    Do something with the trasaction
-              console.log("  tx hash          : " + e.hash + "\n"
-              + "   nonce           : " + e.nonce + "\n"
-              + "   blockHash       : " + e.blockHash + "\n"
-              + "   blockNumber     : " + e.blockNumber + "\n"
-              + "   transactionIndex: " + e.transactionIndex + "\n"
-              + "   from            : " + e.from + "\n"
-              + "   to              : " + e.to + "\n"
-              + "   value           : " + e.value + "\n"
-              + "   time            : " + block.timestamp + " " + new Date(block.timestamp * 1000).toGMTString() + "\n"
-              + "   gasPrice        : " + e.gasPrice + "\n"
-              + "   gas             : " + e.gas + "\n"
-              + "   input           : " + e.input);
+              if ((trackAddress.indexOf(e.from)!=-1)|| (trackAddress.indexOf(e.to)!=-1)){
+                  saveAndDisplay(block,e);
+                }
 
-          }
-      })
-
+              })
     }
     }
 
@@ -49,4 +61,4 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 console.log("Starting Search");
 var eth = web3.eth;
 //console.log(web3.eth.blockNumber);
-getTransactionsByAccount("0x05694d29b7c6c65abce2a7c45317d0e177339d35",1537373,null,1);
+getTransactionsForAccount(1540683,null);
